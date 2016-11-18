5530a440ad2f3cbd23c77c40f3157867ee2a5948
zelh <regulknight@gmail.com>
2016-11-19 01:27:44 +0400

    Adding Task class

--- Model/Task.py
+++ Model/Task.py
@@ -1,4 +1,11 @@
 class Task:
-    def __init__(self, index, category_id, description, location, time, owner, members, status):
-        self.id = index
-        self.category_id = index
+    def __init__(self, index, category_index, description, location, time, owner, status, weight=1):
+        self.index = index
+        self.category_index = category_index
+        self.description = description
+        self.location = location
+        self.time = time
+        self.owner = owner
+        self.members = []
+        self.status = status
+        self.weight = weight
