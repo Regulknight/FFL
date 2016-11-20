4e04ffdf44c9a937a8f12adf7f3ec9c51dbab6e2
Leonid Cheremshantcev <plastic.life.ln@gmail.com>
2016-11-20 08:04:54 +0300

    Minors fix

--- Model/Comment.py
+++ Model/Comment.py
@@ -1,5 +1,6 @@
 class Comment:
-    def __init__(self, text, task_id):
-        self.id = 0
+    def __init__(self, id, text, task_id, owner):
+        self.owner = owner
+        self.id = id
         self.text = text
         self.task_id = task_id
